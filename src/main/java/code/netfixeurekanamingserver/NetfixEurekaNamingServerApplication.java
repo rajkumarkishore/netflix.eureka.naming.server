b3156063a41dea68dfff0cf6443feffbe423814e
Kishore Rajkumar <rajkumar.kishore@gmail.com>
2018-10-26 21:37:51 +0530

    first commit

---
+++ src/main/java/code/netfixeurekanamingserver/NetfixEurekaNamingServerApplication.java
@@ -0,0 +1,12 @@
+package code.netfixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetfixEurekaNamingServerApplication.class, args);
+	}
+}
