f74c448048bca62d3fc1a14620f9e8bd5cbeda2f
Kishore Rajkumar <rajkumar.kishore@gmail.com>
2018-10-27 22:52:21 +0530

    new configuration changes

--- src/main/java/code/netfixeurekanamingserver/NetfixEurekaNamingServerApplication.java
+++ src/main/java/code/netfixeurekanamingserver/NetfixEurekaNamingServerApplication.java
@@ -2,8 +2,10 @@ package code.netfixeurekanamingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NetfixEurekaNamingServerApplication {
 
 	public static void main(String[] args) {
